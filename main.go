d1d820a1bd2bf042d941bc1512d27883d5bc510d
Paul Yu <paul.d.yu@gmail.com>
2022-09-02 15:37:48 -0700

    Update env vars

--- main.go
+++ main.go
@@ -2,10 +2,10 @@ package main
 
 import (
 	"context"
-	"errors"
 	"fmt"
 	"math"
 	"os"
+	"strconv"
 
 	"github.com/Azure/azure-sdk-for-go/sdk/messaging/azservicebus"
 )
@@ -23,48 +23,12 @@ func GetClient() *azservicebus.Client {
 	return client
 }
 
-func SendMessage(message string, client *azservicebus.Client) {
-	sender, err := client.NewSender("myqueue", nil)
-	if err != nil {
-		panic(err)
-	}
-	defer sender.Close(context.TODO())
-
-	sbMessage := &azservicebus.Message{
-		Body: []byte(message),
-	}
-	err = sender.SendMessage(context.TODO(), sbMessage, nil)
-	if err != nil {
-		panic(err)
-	}
-}
-
-func SendMessageBatch(messages []string, client *azservicebus.Client) {
-	sender, err := client.NewSender("myqueue", nil)
-	if err != nil {
-		panic(err)
-	}
-	defer sender.Close(context.TODO())
-
-	batch, err := sender.NewMessageBatch(context.TODO(), nil)
-	if err != nil {
-		panic(err)
-	}
-
-	for _, message := range messages {
-		err := batch.AddMessage(&azservicebus.Message{Body: []byte(message)}, nil)
-		if errors.Is(err, azservicebus.ErrMessageTooLarge) {
-			fmt.Printf("Message batch is full. We should send it and create a new one.\n")
-		}
-	}
-
-	if err := sender.SendMessageBatch(context.TODO(), batch, nil); err != nil {
-		panic(err)
-	}
-}
-
 func GetMessage(count int, client *azservicebus.Client) {
-	receiver, err := client.NewReceiverForQueue("myqueue", nil) 
+	queue, ok := os.LookupEnv("AZURE_SERVICEBUS_QUEUE_NAME") //ex: myqueue
+	if !ok {
+		panic("AZURE_SERVICEBUS_QUEUE_NAME environment variable not found")
+	}
+	receiver, err := client.NewReceiverForQueue(queue, nil) 
 	if err != nil {
 		panic(err)
 	}
@@ -91,7 +55,18 @@ func GetMessage(count int, client *azservicebus.Client) {
 
 func main() {
 	client := GetClient()
+	batchSizeInt := 0
+	
+	batchSize, ok := os.LookupEnv("BATCH_SIZE") //ex: 10
+	if !ok {
+		batchSizeInt = math.MaxInt32
+	}
+
+	batchSizeInt, err := strconv.Atoi(batchSize)
+	if err != nil {
+		batchSizeInt = math.MaxInt32
+	}
 
 	fmt.Println("\nget all messages:")
-	GetMessage(math.MaxInt32, client)
+	GetMessage(batchSizeInt, client)
 }
