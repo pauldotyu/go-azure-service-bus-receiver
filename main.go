92cc58de5bddb04f97cd631ddf12cd198eeffbb7
Paul Yu <paul.d.yu@gmail.com>
2024-02-05 20:52:54 -0800

    Update packages and add azidentity auth

--- main.go
+++ main.go
@@ -3,24 +3,46 @@ package main
 import (
 	"context"
 	"fmt"
+	"log"
 	"math"
 	"os"
 	"strconv"
 
+	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
 	"github.com/Azure/azure-sdk-for-go/sdk/messaging/azservicebus"
 )
 
 func GetClient() *azservicebus.Client {
+	// Use the connection string if it is available, otherwise use Azure Identity
 	connectionString, ok := os.LookupEnv("AZURE_SERVICEBUS_CONNECTION_STRING") //ex: Endpoint=sb://<YOUR_NAMESPACE>.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=<YOUR_SHARED_ACCESS_KEY>
-	if !ok {
-		panic("AZURE_SERVICEBUS_CONNECTION_STRING environment variable not found")
+
+	if ok {
+		client, err := azservicebus.NewClientFromConnectionString(connectionString, nil)
+		if err != nil {
+			log.Fatalf("failed to create a Service Bus client: %v", err)
+		} else {
+			fmt.Println("Service Bus client created from connection string")
+			return client
+		}
+	} else {
+		cred, err := azidentity.NewDefaultAzureCredential(nil)
+		if err != nil {
+			log.Fatalf("failed to obtain a credential: %v", err)
+		}
+		sbHostname, ok := os.LookupEnv("AZURE_SERVICEBUS_HOSTNAME") //ex: <YOUR_NAMESPACE>.servicebus.windows.net
+		if !ok {
+			panic("AZURE_SERVICEBUS_HOSTNAME environment variable not found")
+		}
+		client, err := azservicebus.NewClient(sbHostname, cred, nil)
+		if err != nil {
+			log.Fatalf("failed to create a Service Bus client: %v", err)
+		} else {
+			fmt.Println("Service Bus client created with Azure Identity")
+			return client
+		}
 	}
 
-	client, err := azservicebus.NewClientFromConnectionString(connectionString, nil)
-	if err != nil {
-		panic(err)
-	}
-	return client
+	return nil
 }
 
 func GetMessage(count int, client *azservicebus.Client) {
@@ -28,7 +50,7 @@ func GetMessage(count int, client *azservicebus.Client) {
 	if !ok {
 		panic("AZURE_SERVICEBUS_QUEUE_NAME environment variable not found")
 	}
-	receiver, err := client.NewReceiverForQueue(queue, nil) 
+	receiver, err := client.NewReceiverForQueue(queue, nil)
 	if err != nil {
 		panic(err)
 	}
@@ -56,7 +78,7 @@ func GetMessage(count int, client *azservicebus.Client) {
 func main() {
 	client := GetClient()
 	batchSizeInt := 0
-	
+
 	batchSize, ok := os.LookupEnv("BATCH_SIZE") //ex: 10
 	if !ok {
 		batchSizeInt = math.MaxInt32
@@ -67,7 +89,7 @@ func main() {
 		batchSizeInt = math.MaxInt32
 	}
 
-	fmt.Println("\nget "+ strconv.Itoa(batchSizeInt) +" messages:")
+	fmt.Println("\nget " + strconv.Itoa(batchSizeInt) + " messages at a time:")
 
 	// loop to get messages
 	for {
